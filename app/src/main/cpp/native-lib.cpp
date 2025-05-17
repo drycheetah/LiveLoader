770922369ba4a81aeaa89a1436fde9dc3469ab1b
Livku2 <wellingtonhilbert@outlook.com>
2025-05-17 19:18:36 +0100

    Uploaded Files

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,98 @@
+#include <thread>
+#include "dlfcn.h"
+#include <filesystem>
+#include "fstream"
+#include <fcntl.h>
+#include <android/log.h>
+#include <unistd.h>
+
+using namespace std;
+
+#define _TAG "ModLoader"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,_TAG,__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,_TAG,__VA_ARGS__)
+
+//just makes it easier
+namespace fs = std::__fs::filesystem;
+
+const char *GetPackageName() {
+    char *application_id[256];
+    FILE *fp = fopen("proc/self/cmdline", "r");
+    if (fp) {
+        fread(application_id, sizeof(application_id), 1, fp);
+        fclose(fp);
+    }
+    return (const char *) application_id;
+}
+
+void Load(){
+
+    //get directory that can be easily accessed
+    string localDirectory = string("/storage/emulated/0/Android/data/").append(
+            GetPackageName());
+    //the folder where all mods will be placed
+    string localModsDirectory = localDirectory.append("/nativemods");
+
+    //check if the nativemods folder exists
+    if(!fs::exists(localModsDirectory)) {
+        //if not create the folder
+        fs::create_directory(localModsDirectory.c_str());
+        LOGI("SETUP FINISHED, PLEASE RESTART GAME");
+        //return so the rest of the code does not run
+        return;
+    }
+    //go through every file in the nativemods folder
+    for (const auto& entry : fs::directory_iterator(localModsDirectory)) {
+        //check if it a normal file
+        if (!fs::is_regular_file(entry.status())) {
+            continue;
+        }
+        //make sure it is a native library
+        if(entry.path().extension() != ".so"){
+            continue;
+        }
+
+        //full path of the file in the nativemods folder
+        auto external_path = entry.path().c_str();
+
+        //the games internal data directory
+        auto internalPath = string("/data/data/").append(GetPackageName()).append("/files/").append(entry.path().filename()).c_str();
+
+        //check if an instance of this file exists in the internal directory and if so, delete it
+        if(fs::exists(internalPath)) fs::remove(internalPath);
+
+        //copy the file in the nativemods directory into the internal directory
+        //this is because we are unable to load external files
+        if (!fs::copy_file(external_path, internalPath)) {
+            //if it fails, log an error
+            LOGE("Failed to copy .so to internal storage.");
+            continue;
+        }
+        //load the file with dlopen
+        auto handle = dlopen(internalPath, RTLD_LAZY);
+
+        //log the state
+        if (handle == nullptr) {
+            LOGE("Failed to load %s: %s", entry.path().c_str(), dlerror());
+        }
+        else{
+            LOGI("Successfully loaded %s", entry.path().filename().c_str());
+        }
+    }
+
+}
+__attribute__ ((constructor))
+void lib_main() {
+    std::thread([]() {
+        //wait until il2cpp is loaded
+        void* handle = nullptr;
+        do {
+            handle = dlopen("libil2cpp.so", RTLD_LAZY);
+            sleep(1);
+        } while (handle == nullptr);
+
+        //start the mod loading
+        Load();
+
+    }).detach();
+}
